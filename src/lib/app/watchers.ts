7375febb45e3cd082eaaf3b6820cc5c5c3164aa6
Rus Belkin <fank.tomphson@gmail.com>
2026-08-03 16:01:16 +0300

    watchers model

--- src/lib/app/watchers.ts
+++ src/lib/app/watchers.ts
@@ -1,6 +1,10 @@
-import { action, atom, type Atom, type Ctx, type Unsubscribe } from "@reatom/framework";
+import { action, atom, reatomMap, withReset, type Atom, type Ctx, type Unsubscribe } from "@reatom/framework";
+import { withLog } from "../reatom/extensions";
+
+let watcherId = 0;
 
 export type Watcher<T> = {
+  id: string;
   triggerValue: Atom<T>;
   condition: (value: T) => boolean;
   handler: (ctx: Ctx, value: T) => void | Promise<void>;
@@ -15,6 +19,7 @@ export const watch = <T>(
     handler: (ctx: Ctx, value: T) => void | Promise<void>;
   },
 ): Watcher<T> => ({
+  id: `watcher_${watcherId++}`,
   triggerValue,
   condition: options.condition ?? always,
   handler: options.handler,
@@ -23,26 +28,70 @@ export const watch = <T>(
 const getModelName = (p: string, c: string) => `${p}.watchers.${c}`;
 
 export const watchersModel = ({
-  name, watchers
+  name, watchers: init
 }: {
   name: string, watchers: Watcher<any>[]
 }) => {
-  const $watchersSubs = atom<Unsubscribe[]>([], getModelName(name, "watchersSubs"));
+  const $watchers = atom(
+    init, getModelName(name, "watchers"),
+  ).pipe(
+    withReset(),
+    withLog(),
+  );
+
+  const $watchersSubs = reatomMap<string, Unsubscribe>(
+    new Map(), getModelName(name, "watchersSubs"),
+  ).pipe(
+    withReset(),
+    withLog(),
+  );
 
   const define = action((ctx) => {
-    for (const { triggerValue, condition, handler } of watchers) {
-      const unsub = ctx.subscribe(triggerValue, (value) => {
+    const watchers = ctx.get($watchers);
+
+    for (const watcher of watchers) {
+      const { id, triggerValue, condition, handler } = watcher;
+
+      const rawUnsub = ctx.subscribe(triggerValue, (value) => {
         if (condition(value)) {
           void handler(ctx, value);
         }
       });
 
-      $watchersSubs(ctx, state => [...state, unsub]);
+      let disposed = false;
+
+      const unsub: Unsubscribe = () => {
+        if (disposed) return;
+        disposed = true;
+
+        rawUnsub();
+
+        $watchersSubs.delete(ctx, id);
+        $watchers(ctx, (state) => state.filter((w) => w.id !== id));
+      };
+
+      $watchersSubs.set(ctx, id, unsub);
     }
   }, getModelName(name, "define"))
 
+  const clear = action((ctx) => {
+    for (const [, unsub] of ctx.get($watchersSubs)) {
+      unsub();
+    }
+
+    $watchersSubs.reset(ctx);
+    $watchers.reset(ctx);
+  }, getModelName(name, "clear"))
+
+  const unsub = action((ctx, id: string) => {
+    ctx.get($watchersSubs)?.get(id)?.();
+  }, getModelName(name, "unsub"))
+
   return {
     define,
+    clear,
+    unsub,
     $watchersSubs,
+    $watchers,
   }
 }
